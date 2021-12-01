4cdd730e38b531b0dc969fafd3e73f47a575d0c8
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2021-12-01 21:13:08 +0000

    Add step templates (#6)
    * Added template and script for ARM deployment
    
    * Added ConvertTo-AzureDevOpsVariables script
    
    * Added template & script to check k8s deployment status
    
    * Added continueOnError to kubectl task
    
    * Fixed kubectl arg syntax
    
    * Renamed Test-NamespaceExists.ps1

---
+++ PSScripts/Test-KubernetesDeploymentSucceeded.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$KubectlOutput,
+    [Parameter(Mandatory=$true)]
+    [String]$Deployment
+)
+
+$KubectlObject = ConvertFrom-Json -InputObject $KubectlOutput
+$KubectlObject
+$Deployment = $KubectlObject.items | Where-Object { $_.metadata.name -eq $Deployment }
+Write-Host $($Deployment.metadata.name)
+if ($Deployment.status.availableReplicas -gt 0) { 
+    
+    Write-Output "##vso[task.setvariable variable=DeploymentSucceeded]true" 
+
+}
+else {
+
+    Write-Output "##vso[task.setvariable variable=DeploymentSucceeded]false" 
+
+}
+
+##TO DO: refactor as generalised script for testing k8s output
