5cb71d776e875241a225c724c56e9db4eed0661a
NickGraham101 <nick.graham101@googlemail.com>
2022-02-28 21:50:25 +0000

    Added Export-KeyVaultSecrets

---
+++ PSScripts/Export-KeyVaultSecrets.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+Copies secrets from a Key Vault into a temporary KeyVault.  Returns the secret names and current values as an object.
+
+.DESCRIPTION
+This script is intended to be used with Import-KeyVaultSecrets to copy a KeyVault from one subscription to another (including in a different tenant).  It backups up the secrets 
+from the source Key Vault to a staging Key Vault then returns the name and current value of each secret in an object.  This can be consumed by Import-KeyVaultSecrets which will
+write the secrets to a replacement Key Vault in a different subscription.
+
+This script was designed for working with a hobby project.  Before using it in a production environment you should evaluate whether it meets your security requirements.
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$LocalBackupFolderPath,
+    [Parameter(Mandatory=$true)]
+    [String]$SourceKeyVaultName,
+    [Parameter(Mandatory=$true)]
+    [String]$StagingKeyVaultName
+)
+
+class KeyVaultSecret {
+    [String]$SecretName
+    [String]$SecretValue
+}
+
+$SourceKeyVault = Get-AzKeyVault -VaultName $SourceKeyVaultName
+$StagingKeyVault = Get-AzKeyVault -VaultName $StagingKeyVaultName -ErrorAction SilentlyContinue
+if (!$StagingKeyVault) {
+    Write-Output "Staging Key Vault $StagingKeyVaultName doesn't exist, creating in resource group $($SourceKeyVault.ResourceGroupName)"
+    $StagingKeyVault = New-AzKeyVault -Name $StagingKeyVaultName -ResourceGroupName $SourceKeyVault.ResourceGroupName -Location $SourceKeyVault.Location
+}
+
+$Secrets = Get-AzKeyVaultSecret -VaultName $SourceKeyVaultName
+$SecretValues = @()
+
+foreach ($Secret in $Secrets) {
+    # backup and restore key vault secrets to temp key vault in same subscription
+    $SecretBackup = Backup-AzKeyVaultSecret -VaultName $SourceKeyVault.VaultName -Name $Secret.Name -OutputFile "$LocalBackupFolderPath\$($Secret.Name).blob" -Force
+    Restore-AzKeyVaultSecret -VaultName $StagingKeyVault.VaultName -InputFile $SecretBackup
+    Remove-Item -Path $SecretBackup
+
+    # return secret names and values as an object
+    $SecretValue = Get-AzKeyVaultSecret -VaultName $Secret.VaultName -Name $Secret.Name -AsPlainText
+    $SecretValues += New-Object -TypeName KeyVaultSecret -Property @{ SecretName = $Secret.Name; SecretValue = $SecretValue }
+}
+
+$SecretValues
